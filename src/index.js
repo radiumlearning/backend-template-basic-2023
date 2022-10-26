ad1d3a235eb97d75890e3cc1501573b9b138ef95
Esteban Frare <esteban.frare@radiumrocket.com>
2022-10-26 15:09:48 -0300

    BaSP: implement cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // use "import" to import libraries
 import express from 'express';
+import cors from 'cors';
 
 // use "require" to import JSON files
 const admins = require('./data/admins.json');
@@ -7,6 +8,7 @@ const admins = require('./data/admins.json');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
